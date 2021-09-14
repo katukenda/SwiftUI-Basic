e4fe3fc2b5647229b3ae81d42a9d6e56eaf30ff0
katukenda <katukenda@icloud.coml>
2021-09-14 12:23:59 +0530

    Stocks,padding and spacer

--- SwitfulThinkingBootCamp/SpacerBootCamp.swift
+++ SwitfulThinkingBootCamp/SpacerBootCamp.swift
@@ -9,21 +9,19 @@ import SwiftUI
 
 struct SpacerBootCamp: View {
     var body: some View {
-        HStack{
-            Spacer()
-                .frame(height: 10)
-                .background(Color.red)
+        VStack{
+           
             
             Rectangle()
                 .fill(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
                 .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
            
-            
-            Rectangle()
-                .frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Spacer()
                 .frame(height: 10)
                 .background(Color.red)
+            Rectangle()
+                .frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
             
         }
     }
