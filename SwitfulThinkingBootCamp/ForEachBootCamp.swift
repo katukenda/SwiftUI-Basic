644f6e895e15e34f85bb7bdfabc3673ad2f4fbc0
katukenda <katukenda@icloud.coml>
2021-11-07 23:04:25 +0530

    ForEach loop

---
+++ SwitfulThinkingBootCamp/ForEachBootCamp.swift
@@ -0,0 +1,31 @@
+//
+//  ForEachBootCamp.swift
+//  SwitfulThinkingBootCamp
+//
+//  Created by Janitha Katukenda on 2021-11-07.
+//
+
+import SwiftUI
+
+struct ForEachBootCamp: View {
+    
+    let data: [String] = ["Hi", "janitha", "Prashad", "University"]
+    let myString: String = "Hello"
+    
+    
+    var body: some View {
+        VStack{
+            ForEach(data.indices) {
+                index in
+                Text("\(data[index]): \(index)")
+            }
+          
+        }
+    }
+}
+
+struct ForEachBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEachBootCamp()
+    }
+}
